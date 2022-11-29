6bfb4fe77a9a0f174d8a89149c1e72051b2e9649
Brandon Bodnar <bodnarbm@gmail.com>
2022-11-29 13:07:58 -0500

    after vga-text-mode
    Related post: https://os.phil-opp.com/vga-text-mode/

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,20 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &str = "Hello World!";
-static COLOR_BYTE_LIGHT_CYAN: u8 = 0xb;
+mod vga_buffer;
 
 /// Panic handler
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 /// Entry point function
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello World{}", "!");
 
-    for (i, byte) in HELLO.bytes().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = COLOR_BYTE_LIGHT_CYAN;
-        }
-    }
-
+    #[allow(clippy::empty_loop)]
     loop {}
 }
